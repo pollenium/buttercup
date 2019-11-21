13cf26ed78296d1333fee8d3cb3d984240d8440d
Aakil Fernandes <aakilfernandes@gmail.com>
2019-11-21 17:11:38 -0500

    expose Address in index

--- node/index.d.ts
+++ node/index.d.ts
@@ -2,5 +2,6 @@ export { Buttercup } from './buttercups/Buttercup';
 export { DynButtercup } from './buttercups/DynButtercup';
 export { FixLeftButtercup, FixRightButtercup } from './buttercups/fixButtercups';
 export { Bytes } from './externals/Bytes';
+export { Address } from './externals/Address';
 export { Uint8, Uint16, Uint24, Uint32, Uint40, Uint48, Uint56, Uint64, Uint72, Uint80, Uint88, Uint96, Uint104, Uint112, Uint120, Uint128, Uint136, Uint144, Uint152, Uint160, Uint168, Uint176, Uint184, Uint192, Uint200, Uint208, Uint216, Uint224, Uint232, Uint240, Uint248, Uint256 } from './externals/uintXs';
 export { Bytes1, Bytes2, Bytes3, Bytes4, Bytes5, Bytes6, Bytes7, Bytes8, Bytes9, Bytes10, Bytes11, Bytes12, Bytes13, Bytes14, Bytes15, Bytes16, Bytes17, Bytes18, Bytes19, Bytes20, Bytes21, Bytes22, Bytes23, Bytes24, Bytes25, Bytes26, Bytes27, Bytes28, Bytes29, Bytes30, Bytes31, Bytes32 } from './externals/bytesXs';
--- templates/index.ts
+++ templates/index.ts
@@ -2,6 +2,7 @@ export { Buttercup } from './buttercups/Buttercup'
 export { DynButtercup } from './buttercups/DynButtercup'
 export { FixLeftButtercup, FixRightButtercup } from './buttercups/fixButtercups'
 export { Bytes } from './externals/Bytes'
+export { Address } from './externals/Address'
 export {
   {{#each uintXClasses}}
   Uint{{bits}}{{#unless @last}},{{/unless}}
